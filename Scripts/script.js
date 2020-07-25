ec4dcb05d1689850bad47460038e19c731f356b2
Raman Mangla <ramanmangla7@gmail.com>
2020-07-26 01:48:31 +0530

    Add banner content

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,24 @@
 // Responsive navigation
+const sidebar = $(".sidebar");
+
 $("#navIcon").on("click", function () {
-  $(".header-sidebar").addClass("header-sidebar--open");
+  sidebar.addClass("sidebar--open");
 
   $(".header-nav-link").on("click", function () {
-    $(".header-sidebar").removeClass("header-sidebar--open");
+    sidebar.removeClass("sidebar--open");
   });
 
   $(window).resize(function () {
-    $(".header-sidebar").removeClass("header-sidebar--open");
+    sidebar.removeClass("sidebar--open");
   });
 
   $(document).mouseup(function (e) {
-    var menu = $(".header-sidebar");
-    if (!menu.is(e.target) && menu.has(e.target).length === 0) {
-      menu.removeClass("header-sidebar--open");
+    if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0) {
+      sidebar.removeClass("sidebar--open");
     }
   });
 });
 
 $("#closeNavIcon").on("click", function () {
-  $(".header-sidebar").removeClass("header-sidebar--open");
+  sidebar.removeClass("sidebar--open");
 });
