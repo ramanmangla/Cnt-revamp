7e8496a647ccf32dae7ea123924e32f325ef11cc
Raman Mangla <ramanmangla7@gmail.com>
2020-08-05 20:14:29 +0530

    Add services page complete

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,24 @@
-// Responsive navigation
+// Toggle Nav on scroll
+var lastScroll = 0;
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 90) {
+    $("header").addClass("header--shadow");
+
+    if ($(window).scrollTop() < lastScroll) {
+      $("header").removeClass("header--hide");
+    } else {
+      $("header").addClass("header--hide");
+    }
+  } else {
+    $("header").removeClass("header--shadow");
+    $("header").removeClass("header--hide");
+  }
+
+  lastScroll = $(window).scrollTop();
+});
+
+// Screen Responsive Nav and Sidebar
 const sidebar = $(".sidebar");
 
 $("#navIcon").on("click", function () {
@@ -22,3 +42,41 @@ $("#navIcon").on("click", function () {
 $("#closeNavIcon").on("click", function () {
   sidebar.removeClass("sidebar--open");
 });
+
+// Smooth Scroll
+$(".sidebar-link").on("click", function (e) {
+  var targetHref = $(this).attr("href");
+  // var offset = 0;
+  // offset = targetHref == '#Projects' ? -40 : 0;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(targetHref).offset().top - 20,
+    },
+    1000
+  );
+
+  e.preventDefault();
+});
+
+$(".header-nav-link").on("click", function (e) {
+  var targetHref = $(this).attr("href");
+  // var offset = 0;
+  // offset = targetHref == '#Projects' ? -40 : 0;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(targetHref).offset().top - 0,
+    },
+    1000
+  );
+
+  e.preventDefault();
+});
+
+display: grid;
+//   grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+//   justify-items: center;
+//   align-items: center;
+//   grid-gap: 30px;
+//   row-gap: 35px;
